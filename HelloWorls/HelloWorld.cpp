574ac1bf9c0f4862e458092a6f60ea081b543204
Keshav Sharma <sharmakeshav802@gmail.com>
2025-12-26 13:01:38 +0530

    changes for array and function

--- HelloWorls/HelloWorld.cpp
+++ HelloWorls/HelloWorld.cpp
@@ -3,10 +3,26 @@
 //The iostream is the header file that defines functions and operations related to the input / output stream
 using namespace std;  //Namespace
 
+
+int add(int a, int b);
+
+
+void changeValue(int x) {
+	x = 100;  // modifies local copy
+	cout << x << endl;
+}
+
+
+void changeValue_(int& x) {  // reference parameter
+	x = 100;  // modifies original variable
+	cout << x << endl;
+}
+
+
 int  main() //The main() function is the default starting point of any C++ program.
 {
 	//cout  --Printing Statement 
-	cout << "this is first line"<<endl;
+	//cout << "this is first line"<<endl;
 
 	// variable- it is like a container which store value,
 	// like a=10 , so a is container stroing 10 as a value
@@ -32,38 +48,38 @@ int  main() //The main() function is the default starting point of any C++ progr
 		long double				12bytes
 		wchar_t					2 or 4 bytes	1 wide character*/
 
-	int p2 = 10.5;
+	//int p2 = 10.5;
 	/*int p1{ 10.5 };
 	int p3(10.5);*/
-	cout << "Integer:p " << p2 << endl;
+	//cout << "Integer:p " << p2 << endl;
 	/*cout << "Integer:p1 " << p1 << endl;
 	cout << "Integer:p3 " << p3 << endl;*/
 
 
-	char ch = 'A';
+	/*char ch = 'A';
 	int i = 10;
 	float f = 10.5;
 	double d = 99.99;
 	bool flag = true;
-	const double MY_PI = 3.14159;
+	const double MY_PI = 3.14159;*/
 
 	//
 
-	cout << "Character: " << ch << endl;
-	cout << "Integer: " << i << endl;
-	cout << "Float: " << f << endl;
-	cout << "Double: " << d << endl;
-	cout << "Boolean: " << flag << endl;
-	cout << boolalpha; // note
-	cout << "Boolean: " << flag << endl;
-	cout << "MY_PI: " << MY_PI << endl;
+	//cout << "Character: " << ch << endl;
+	//cout << "Integer: " << i << endl;
+	//cout << "Float: " << f << endl;
+	//cout << "Double: " << d << endl;
+	//cout << "Boolean: " << flag << endl;
+	//cout << boolalpha; // note
+	//cout << "Boolean: " << flag << endl;
+	//cout << "MY_PI: " << MY_PI << endl;
 
-	////to find size of a varaible we can use sizeof() function
-	int x = 10;
-	char ch1 = 'A';
+	//////to find size of a varaible we can use sizeof() function
+	//int x = 10;
+	//char ch1 = 'A';
 
-	cout << sizeof(x) << endl;
-	cout << sizeof(ch1) << endl;
+	//cout << sizeof(x) << endl;
+	//cout << sizeof(ch1) << endl;
 
 	////Oprators
 	////1. Arithmetic Operators
@@ -74,16 +90,16 @@ int  main() //The main() function is the default starting point of any C++ progr
 	//		 /: Division            
 	//		 % : Modulus(remainder) 
 	//	 */
-	int a = 12, b = 8;
+	//int a = 12, b = 8;
 
 
 	//cout << "Sum a and b, a  is  " << a << "and b is =" << b << "a+b=" << a + b << endl;
 
-	cout << "Sum = " << a + b << endl;
+	/*cout << "Sum = " << a + b << endl;
 	cout << "Difference = " << a - b << endl;
 	cout << "Product = " << a * b << endl;
 	cout << "Division = " << a / b << endl;
-	cout << "Remainder = " << a % b << endl;
+	cout << "Remainder = " << a % b << endl;*/
 
 	//2. Relational Operators  
 	     /*
@@ -94,9 +110,9 @@ int  main() //The main() function is the default starting point of any C++ progr
 			 >=       : Greater than or equal 
 			 <=       : Less than or equal 
 		 */
-	int y = 10, z = 20;
+	/*int y = 10, z = 20;
 
-	cout << (y < z) << endl;
+	cout << (y < z) << endl;*/
 
 	//3.Logical Operators
 	///*
@@ -106,9 +122,9 @@ int  main() //The main() function is the default starting point of any C++ progr
 	//	 !		 : Logical NOT 
 	//*/
 
-		int p = 10, q = 20;
+		/*int p = 10, q = 20;
 
-		cout << (p < q && q > 15) << endl;
+		cout << (p < q && q > 15) << endl;*/
 
 
 	//
@@ -130,9 +146,9 @@ int  main() //The main() function is the default starting point of any C++ progr
 	//	| -- | Decrement |
 	//*/
 
-	int m = 10;
+	/*int m = 10;
 	m++;
-	cout << m << endl;
+	cout << m << endl;*/
 
 
 	
@@ -159,42 +175,42 @@ int  main() //The main() function is the default starting point of any C++ progr
 
 
 	//string
-	string str = "Hello, World!";
-	cout << str << endl;
-	cout << str[0] << endl; //indexing 
-	cout << str.at(3) << endl; //using at() method
-	cout << str.size() << endl; //size of string
-	cout << str.length() << endl; //length of string
+	//string str = "Hello, World!";
+	//cout << str << endl;
+	//cout << str[0] << endl; //indexing 
+	//cout << str.at(3) << endl; //using at() method
+	//cout << str.size() << endl; //size of string
+	//cout << str.length() << endl; //length of string
 
-	//Concatenation string
-	string name = "keshav";
-	string greeting = "Hello " + name;
-	cout << greeting << endl;
+	////Concatenation string
+	//string name = "keshav";
+	//string greeting = "Hello " + name;
+	//cout << greeting << endl;
 
-	//addding char in string
-	name.push_back('!');
-	cout << name << endl;
+	////addding char in string
+	//name.push_back('!');
+	//cout << name << endl;
 
-	//Removing last char from string
-	name.pop_back();
-	cout << name << endl;
+	////Removing last char from string
+	//name.pop_back();
+	//cout << name << endl;
 
 
-	//inserting sub string
-	name.insert(6, " sharma");
-	cout << name << endl;
+	////inserting sub string
+	//name.insert(6, " sharma");
+	//cout << name << endl;
 
-	//erasing sub string
-	name.erase(6, 4);
-	cout << name << endl;
+	////erasing sub string
+	//name.erase(6, 4);
+	//cout << name << endl;
 
-	// substring
-	string sub = name.substr(0, 6);
-	cout << sub << endl;
+	//// substring
+	//string sub = name.substr(0, 6);
+	//cout << sub << endl;
 
-	//string find
-	//name.find("keshav");
-	cout << name.find("keshav") << endl;
+	////string find
+	////name.find("keshav");
+	//cout << name.find("keshav") << endl;
 
 	//Input
 
@@ -213,7 +229,7 @@ int  main() //The main() function is the default starting point of any C++ progr
 
 	// conditions-> if ,else.else if.
 
-	int yourage = 16;
+	/*int yourage = 16;
 	if (yourage > 18)
 		cout << "please go and vote" << endl;;
 
@@ -236,35 +252,87 @@ int  main() //The main() function is the default starting point of any C++ progr
 	}
 	else {
 		cout << "Fail" << endl;
-	}
+	}*/
 
 
 	//switch 
-	int day = 3;
-		switch (day) {
-		case 1:cout << "monday"; break;
-		case 2: cout << "Tuesday"; break;
-		case 3: cout << "Wednesday"; break;
-		default: cout << "Invalid day";
+	//int day = 3;
+	//	switch (day) {
+	//	case 1:cout << "monday"; break;
+	//	case 2: cout << "Tuesday"; break;
+	//	case 3: cout << "Wednesday"; break;
+	//	default: cout << "Invalid day";
 
-		}
+	//	}
 
-	//loops
-		/*for (int i = 0;i <=5;i++) {
-			cout << i << endl;
-		}*/
+	////loops
+	//	for (int i = 0;i <=5;i++) {
+	//		cout << i << endl;
+	//	}
 
-		//do while
-		/*int l = 0;
-		do {
-			cout << l << endl;
-			l++;
-		} while (l < 5);*/
+	//	//do while
+	//	int l = 0;
+	//	do {
+	//		cout << l << endl;
+	//		l++;
+	//	} while (l < 5);
 
-		//while
-		int l = 0;
-		while (l <= 5) {
-			cout << l << endl;
-			l++;
-		}
+	//	//while
+	//	int l = 0;
+	//	while (l <= 5) {
+	//		cout << l << endl;
+	//		l++;
+	//	}
+
+
+	//Array:
+	//-An array is a collection of elements of the same type stored in contiguous memory locations.
+	//- It allows storing multiple values under a single name, accessed using an index.
+
+	//int arr[5];              // declaration
+	int arr[5] = { 10, 20, 30, 40, 50 };  // initialization
+	//int arr[] = { 1, 2, 3 };   // compiler decides size (3 here)
+
+
+	cout << arr[0]<<endl;  // prints first element
+	arr[2] = 100;    // updates third element
+	cout << arr[2]<<endl;  // prints first element
+	
+	//taking array as input
+	int n;
+	cout << "Enter size of array"<<endl;
+	cin >> n;
+	int arr1[100];
+	cout << "enter array element"<<endl;
+	for (int i = 0; i < n; i++) {
+		cin >> arr1[i];   // input
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << arr1[i] << " ";  // output
+	}
+	
+	//matrix or 2d array
+	int mat[3][3] = {
+		{1,2,3},
+		{4,5,6},
+		{7,8,9}
+	};
+
+	cout << mat[0][1];
+
+	//Function : - A function is a block of code that performs a specific task.
+	//-Helps in modularity, reusability, and readability of code.
+
+
+	cout << add(3, 4);  // Function Call
+	int a = 10;
+	changeValue(a); //call by value
+	cout << a << endl;
+	changeValue_(a);
+	cout << a << " orignal a" << endl;
+}
+
+int add(int a, int b) {
+	return a + b;
 }
